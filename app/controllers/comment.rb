70253e62a22394c244106444ded358a1232e35bc
Kyle Dorsey <KylePDorsey@gmail.com>
2016-09-21 16:01:50 -0500

    Added comment page for questions

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -4,22 +4,25 @@
 # post
 # answer/:id/comments/new
 
-get '/question/:id/comments/new' do
+get '/questions/:id/comments/new' do
   @question = Question.find(params[:id])
   erb :'/comments/new'
 end
 
 
-post '/question/:id/comments' do
-  Comment.new(commentable_type: 'Question', )
-  erb :'/comments/new'
+post '/questions/:id/comments' do
+  @question = Question.find(params[:id])
+  p params[:id]
+  comment = Comment.create(text: params[:comment_text], commentable_type: 'Question', commentable_id: params[:id], user_id: logged_in_user.id)
+  p comment
+  redirect '/' #redirect to question page with
 end
 
 
-get '/answer/:id/comments/new' do
+get '/answers/:id/comments/new' do
   erb :'/comments/new'
 end
 
-post '/answer/:id/comments' do
-  erb :'/comments/new'
+post '/answers/:id/comments' do
+  erb :'/home'
 end
