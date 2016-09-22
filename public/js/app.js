32e6bce08243cd2a09afe817c3159021c3363782
Caitlin Johnson & Greg Garner <pair+caitlinlikesrobots.garner412@devbootcamp.com>
2016-09-22 16:29:10 -0500

    tihngs and stuff ??

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.container').on('submit', '.upvotes', function(event) {
     event.preventDefault();
-    var $form = $(this)
-    var url = $form.attr('action')
-    var method = $form.attr('method')
+    var $form = $(this);
+    var url = $form.attr('action');
+    var method = $form.attr('method');
   $.ajax({
     url: url,
     method: method,
@@ -11,6 +11,11 @@ $(document).ready(function() {
   })
   .done(function(response) {
     $form.find(".upvotes").text(response);
-  })
-  })
+  });
+  });
+
+  $('a').on('click', function() {
+    event.preventDefault();
+
+  });
 });
