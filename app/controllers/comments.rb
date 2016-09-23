336a4c730951b541ebf1ed22952233f588f027b8
Greg Garner <gpgarner10@gmail.com>
2016-09-23 00:29:10 -0500

    fix comment ajax comment issues... Question comment is not ajaxing

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -12,7 +12,11 @@ end
 post '/questions/:id/comments' do
   @question = Question.find(params[:id])
   comment = Comment.create(text: params[:comment_text], user_id: logged_in_user.id,  commentable_id: params[:id], commentable_type: 'Question')
-  redirect "/questions/#{params[:id]}"
+  if request.xhr?
+    erb :"/comments/_question_comment", layout: false, locals: {question: @question, qcomment: comment}
+  else
+    redirect "/questions/#{params[:id]}"
+  end
 end
 
 post '/answers/:id/comments/new' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $('.new_comment_button').on('click', function(event){
+  $('.new_comment_button-q').on('click', function(event){
     event.preventDefault();
     var $form = $(this);
     $form.hide();
     // $form.siblings('form.new_comment_form')
-    $('.new_comment_form').removeClass('hidden');
+    $('.new_comment_form-q').removeClass('hidden');
   });
-  $('.new_comment_form').on('submit', function(event){
+  $('.container').on('submit', '.new_comment_form-q', function(event){
     event.preventDefault();
     var form = $(this);
     var url = form.attr('action');
@@ -19,10 +19,36 @@ $(document).ready(function() {
       data: data
     })
     .done(function(response){
-      // console.log(response)
+      debugger;
+      $('.append_comment_q').append(response);
+      form.removeClass('hidden');
+    })
+  })
+
+  $('.new_comment_button-a').on('click', function(event){
+    event.preventDefault();
+    var $form = $(this);
+    $form.hide();
+    // $form.siblings('form.new_comment_form')
+    $('.new_comment_form-a').removeClass('hidden');
+  });
+  $('.new_comment_form-a').on('submit', function(event){
+    event.preventDefault();
+    var form = $(this);
+    var url = form.attr('action');
+    var method = form.attr('method');
+    var data = form.serialize();
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+    })
+    .done(function(response){
       $('.append_comment').append(response);
       form.removeClass('hidden');
     })
   })
+
 
 });
